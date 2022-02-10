a1a854b755aa84881b4895789aab15c4649dfc12
dar4kamal <sasaa9646@gmail.com>
2022-02-10 10:35:56 +0200

    setup API Docs using Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { UniversalExceptionFilter } from './utilities/universal.filter';
@@ -11,6 +12,19 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new UniversalExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Readwise-clone API')
+    .setDescription('Readwise-clone API Documentation')
+    .setVersion('1.0')
+    .addBasicAuth(
+      { type: 'apiKey', in: 'header', name: 'x-auth-token' },
+      'x-auth-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(5000);
 }
